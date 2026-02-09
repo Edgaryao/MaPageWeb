15f5f0c94c8884256d85654e4f5843a05f21c180
Edgaryao <Yaoyerockedgar@gmail.com>
2026-02-09 13:45:11 +0000

    Update welcome message logic based on time

--- asset/Js/message_bienvenue.js
+++ asset/Js/message_bienvenue.js
@@ -9,15 +9,18 @@ document.addEventListener("DOMContentLoaded", () => {
     const hour = new Date().getHours();
     let message = "";
 
-    if (hour >= 5 && hour < 12) {
+    if (hour >= 6 && hour < 12) {
       message =
         "Ravi de vous accueillir en début de journée. Nous vous souhaitons agréable visite du site.";
-    } else if (hour >= 12 && hour < 18) {
+    else if (hour >= 12 && hour < 14) {
       message =
-        "Merci de votre visite à ce moment clé de la journée. Nous vous souhaitons agréable visite du site. Passez un bon après midi.";
+        "Ravi de vous accueillir a ce moment clé de la journée dédié a la pause. Nous vous souhaitons agréable visite du site.";
+    } else if (hour >= 14 && hour < 18) {
+      message =
+        "Merci de votre visite , Nous vous souhaitons agréable visite du site. Passez un bon après midi.";
     } else if (hour >= 18 && hour < 22) {
       message =
-        "Merci de prendre ce temps pour découvrir mon travail.Passez une bonne soirée.";
+        "Merci de votre visite à ce moment clé de la journée. Nous vous souhaitons agréable visite du site et une bonne soirée.";
     } else {
       message =
         "Merci de consulter mon profil dans un moment plus calme. Le développement demande parfois concentration et précision.";
@@ -43,3 +46,4 @@ document.addEventListener("DOMContentLoaded", () => {
     overlay.classList.remove("show");
   });
 });
+
